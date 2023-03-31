cfded2694aae766d65189b3713607bb7054c55b4
Tyler Trinh <bosaget101@outlook.com>
2023-03-30 19:48:10 -0700

    Changed a word and editted some js stuff

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
-console.log("Hello world lmao");
+console.log("Hello world lol");
+
+var ten = 10;
+
+console.log(ten);
+
+printTime();
+
+
+function printTime() {
+    const date = new Date();
+    var calendarDay = date.getFullYear() + '-' + (date.getMonth()+1) + '-' + date.getDate();
+
+    var currTime = date.getHours() + ':' + date.getMinutes() +':' + date.getSeconds();
+    console.log(calendarDay);
+    console.log("Time website opened: "+currTime);
+}
