0dbcd33ad41942a3ee7ca38182ee7cbc8c8eacde
“Basilisk00” <bosaget101@gmail.com>
2024-02-18 22:44:44 -0800

    Started progress on canvas

--- game.js
+++ game.js
@@ -1,18 +1,26 @@
 var birb;
+var image = new Image();
+image.src = "https://github.com/Basilisk00/Basilisk00.github.io/blob/main/docs/assests/img/birb_cropped.png?raw=true";
 
 function startGame() {
     myGameArea.start();
+    birb = new player();
   }
   
   // Object that creates canvas 
   var myGameArea = {
     canvas : document.createElement("canvas"),
     start : function() {
-      this.canvas.width = 550;
+      this.canvas.width = 550; // Canvas dimensions
       this.canvas.height = 600;
-      this.context = this.canvas.getContext("2d"); // create drawing  element
-      document.body.insertBefore(this.canvas, document.body.childNodes[7]);
+      this.context = this.canvas.getContext("2d"); // create drawing element
+      document.body.insertBefore(this.canvas, document.body.childNodes[1]); // Place canvas as second element
     }
+  }
+
+  function player() {
+  myGameArea.context.drawImage(image, 230, 240, 90, 130); // First pair is coords, other pair of num is dimension of img
+    
   }
 
   
